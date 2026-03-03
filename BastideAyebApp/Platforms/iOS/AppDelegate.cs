75d35811ca1eb9933ce9ba9a4013be518e292411
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-03-03 15:39:28 +0100

    Initial commit: Ajout du projet .NET MAUI

---
+++ BastideAyebApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BastideAyebApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
