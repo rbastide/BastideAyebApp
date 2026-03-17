416bd156cec71eeb5b436913aff5493ece5cf90f
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-03-17 14:58:36 +0100

    Ajout des 2 classes d'api

---
+++ BastideAyebApp/Models/CardModels.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BastideAyebApp.Models;
+
+public class CardModels
+{
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+
+    [JsonPropertyName("value")]
+    public string Value { get; set; }
+
+    [JsonPropertyName("suit")]
+    public string Suit { get; set; }
+    
+}
+
+public class DeckResponse
+{
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonPropertyName("cards")]
+    public List<CardModels> Cards { get; set; }
+}
---
+++ BastideAyebApp/Services/CardService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Json;
+using BastideAyebApp.Models;
+
+namespace BastideAyebApp.Services;
+
+public class CardService
+{
+    
+    private readonly HttpClient _httpClient;
+
+    public CardService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<List<CardModels>> GetCardsAsync(int count = 10)
+    {
+        try
+        {
+            var url = $"https://deckofcardsapi.com/api/deck/new/draw/?count={count}";
+            
+            var response = await _httpClient.GetFromJsonAsync<DeckResponse>(url);
+
+            if (response != null && response.Success)
+            {
+                return response.Cards;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur lors de l'appel API : {ex.Message}");
+        }
+
+        return new List<CardModels>();
+    }
+    
+}
